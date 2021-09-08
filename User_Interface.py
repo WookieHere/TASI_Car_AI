0c9f875ad94c54a58691dc86054866e81732148e
WookieHere <47507330+WookieHere@users.noreply.github.com>
2021-09-07 23:34:38 -0400

    takes in keyboard input now for manual driving

--- User_Interface.py
+++ User_Interface.py
@@ -1,25 +1,54 @@
 import array
+import Messager_Facade
 
 class UserInterface:
-    def __init__(self, Graphics):
-        self.GH = Graphics
-        self.valid_keys = ["w", "a", "s", "d", "p"]
+    def __init__(self, Messager):
+        self.Type = "User"
+        self.Msgr = Messager
+        self.GH = Messager.Graphics
+        self.valid_keys = ["w", "a", "s", "d", "o", "p", "r"]
+        self.cmdList = []
         self.funcDict = {
             0: "self.changeSpeed(1)",
             1: "self.changeDir(1)",
             2: "self.changeSpeed(-1)",
             3: "self.changeDir(-1)",
-            4: "end"
+            4: "self.changeMode('MapEdit')",
+            5: "self.changeMode('Pause')",
+            6: "self.changeMode('Run')",
+            7: "end"
         }
 
 
     def checkInput(self):
         keypressed = self.GH.window.checkKey()
         if keypressed != "":
-            return self.valid_keys.index(keypressed)
+            print(keypressed)
+            try:
+                return self.valid_keys.index(keypressed)
+            except (ValueError):
+                print("Warning: Invalid Key Pressed")
+                return len(self.funcDict) - 1  # should return "end"
         else:
-            return 4
+            return len(self.funcDict) - 1 #should return "end"
+
+    def checkMouseInput(self):
+        key = self.GH.window.checkKey()
+        mousePoint = self.GH.window.checkMouse()
+        if  key == "e":
+            return -1
+        elif key == "c":
+            self.createCar(mousePoint)
+            return -1
+        else:
+            return mousePoint
 
     def processInput(self):
-        self.GH.addToEvents(self.funcDict[self.checkInput()])
+        self.cmdList.append(self.funcDict[self.checkInput()])
+
+    def pullInput(self):
+        return self.cmdList
+
+    def createCar(self, point):
+        self.cmdList.append("self.addUserObject(Car(Point(" + str(point.x) + "," + str(point.y) + ")))")
 
